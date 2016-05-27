005edb47ebdb1b070aa1eaa592559a3028c2eb06
oh_minjung <oh_minjung@cyberagent.co.jp>
2016-05-27 09:50:08 +0900

    new()

---
+++ new/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Task struct {
+	ID int
+	Detail string
+	done bool
+}
+
+func main() {
+	var task *Task = new(Task)
+	task.ID = 1
+	task.Detail = "buy the milk"
+	fmt.Println(task.done) // false
+}
