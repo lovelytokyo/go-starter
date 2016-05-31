b5b3eabc29e2b2cff3aa2a66a72c2a038cccfabd
oh_minjung <oh_minjung@cyberagent.co.jp>
2016-05-31 11:16:34 +0900

    write file by json\s encoder/decoder

--- json/main.go
+++ json/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"fmt"
+	"os"
 )
 
 type Person struct {
@@ -22,6 +23,26 @@ func main() {
 		memo : "golang lover",
 	}
 
+	/* write json to file by json encoder */
+	// open file
+	file, err := os.Create("./person.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// get encoder
+	encoder := json.NewEncoder(file)
+
+	// encode json and write to file
+	err = encoder.Encode(person)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//$ cat person.json                                                                                                                                                    (master✱)
+	//{"id":1,"name":"Gopher"}
+
+
 	b, err := json.Marshal(person)
 	if err != nil {
 		log.Fatal(err)
@@ -37,4 +58,24 @@ func main() {
 		log.Fatal(err2)
 	}
 	fmt.Println(person2)
+
+	/* read file and decode string to json */
+	file3, err := os.Open("./person.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file3.Close()
+
+	var person3 Person
+
+	// get data
+	decoder := json.NewDecoder(file3)
+
+	// write json data to type
+	err = decoder.Decode(&person3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(person3)
 }
